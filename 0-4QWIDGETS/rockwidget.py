91fee01769902d46bd6b69cb9e72ce2add029832
Aly Mustafa <109925614+AlyMustafa@users.noreply.github.com>
2024-12-14 18:22:46 +0200

    Update rockwidget.py

--- 0-4QWIDGETS/rockwidget.py
+++ 0-4QWIDGETS/rockwidget.py
@@ -3,13 +3,20 @@ from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
 class RockWidget (QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Aya Mustafa")
+        self.setWindowTitle("PWM")
 
-        button1 = QPushButton("اضربها")
-        button2 = QPushButton("بوسها")
+        button1 = QPushButton("Duty")
+        button1.clicked.connect(self.button1_clicked)
+        button2 = QPushButton("Bob")
+        button2.clicked.connect(self.button2_clicked)
 
         button_layout = QHBoxLayout()
         button_layout.addWidget(button1)
         button_layout.addWidget(button2)
 
         self.setLayout(button_layout)
+
+    def button1_clicked(self):
+        print("antesh")
+    def button2_clicked(self):
+        print("Arza3")
